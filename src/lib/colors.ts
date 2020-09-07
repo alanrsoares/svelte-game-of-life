0eb8a8cf891eb2b2c227aa44439403b420ab7722
Alan R. Soares <alanrsoars@gmail.com>
2020-09-08 00:12:13 +1200

    convert to private fields

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,29 +1,29 @@
 import { GRID_SIZE } from "./config";
 
 class HSLGenerator {
-  private hueIncrement: number;
-  private saturation: number;
-  private lightness: number;
-  private cache: Record<string, string> = {};
+  #hueIncrement: number;
+  #saturation: number;
+  #lightness: number;
+  #cache: Record<string, string> = {};
 
   constructor(hueLength = 100, options = { lightness: 0.6, saturation: 1 }) {
-    this.hueIncrement = 360 / Math.sqrt(hueLength ** 2 * 2);
-    this.saturation = options.saturation * 100;
-    this.lightness = options.lightness * 100;
-    this.cache = {};
+    this.#hueIncrement = 360 / Math.sqrt(hueLength ** 2 * 2);
+    this.#saturation = options.saturation * 100;
+    this.#lightness = options.lightness * 100;
+    this.#cache = {};
   }
 
-  getColor(y: number, x: number) {
+  public getColor(y: number, x: number) {
     const cacheKey = [y, x].join("-");
 
-    if (cacheKey in this.cache) {
-      return this.cache[cacheKey];
+    if (cacheKey in this.#cache) {
+      return this.#cache[cacheKey];
     }
 
-    const hue = Math.floor(Math.sqrt(y ** 2 + x ** 2) * this.hueIncrement);
-    const hslColor = `hsl(${hue},${this.saturation}%,${this.lightness}%)`;
+    const hue = Math.floor(Math.sqrt(y ** 2 + x ** 2) * this.#hueIncrement);
+    const hslColor = `hsl(${hue},${this.#saturation}%,${this.#lightness}%)`;
 
-    this.cache[cacheKey] = hslColor;
+    this.#cache[cacheKey] = hslColor;
 
     return hslColor;
   }
